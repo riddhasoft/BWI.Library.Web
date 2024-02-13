a34f861f69e66c57bfe6650b84eb7827b0b0722c
Binod sapkota <binodsapkota96@gmail.com>
2024-02-13 16:40:58 +0545

    init

---
+++ BWI.Library.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BWI.Library.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
