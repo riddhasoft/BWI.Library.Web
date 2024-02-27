1ed533f6d04a2f3d8b95ef75620948fa9e6530cd
Umesh SHRESTHA <umeshshresthanp@gmail.com>
2024-02-27 16:41:59 +0545

    Entities update

---
+++ BWI.Library.Entities/BookModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BWI.Library.Entities
+{
+    internal class BookModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateOnly PublicationDate { get; set; }
+        public string Edition { get; set; }
+        public bool IsActive { get; set; }
+        public string Code { get; set; }
+        [ForeignKey("BookCategory")]
+        public int BookCategoryId { get; set; }
+        [ValidateNever]
+        public virtual BookCategoryModel BookCategory { get; set; }
+    }
+}
--- BWI.Library.Entities/StudentModel.cs
+++ BWI.Library.Entities/StudentModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,13 @@ namespace BWI.Library.Entities
 {
     public class StudentModel
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string RollNo { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime RegisteredDate { get; set; } = DateTime.Now;
+        public DateTime ExpiredDate { get; set; }
+
     }
 }
